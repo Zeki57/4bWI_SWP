d8449633aa84f011c0603be1ab850a072f0310ff
zeki57 <zeki.aksoy05@gmail.com>
2024-11-12 08:54:23 +0100

    01_JS_Variablen_Operationen DONE

---
+++ 01_JS Variablen_Operationen/script.js
@@ -0,0 +1,49 @@
+console.log("Hallo");
+
+console.log("Tolle Sache");
+
+// Variablen 
+
+let a = 7;
+
+let b = 8;
+
+let c = 10;
+
+// Berechne folgendes und gib das Eregebnis über console.log aus 
+
+let result = a + b + c;
+console.log("Das Ergebniss ist = " + result);
+// a + b + c 
+result = (a + b) / c;
+console.log("Ergebniss: " + result);
+// (a + b) / c 
+result = c - a + b;
+console.log("10 - 7 + 8 = " + result);
+// c -a + b 
+result = a + b + c * 3
+console.log("Das richtige Nummer wäre: " + result);
+// a + b + c * 3 
+result = (a + b + c) / 4
+console.log("(7 + 8 + 10)/4 = " + result);
+// (a + b + c)/4 
+
+
+
+// Ändere die Variablen auf folgende Werte 
+
+// a = 3 
+
+// b = a + 3 
+
+// c = c -3 
+
+// gib die neuen Werte der Variablen aus 
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+console.log("a hat den wert = " + a, ",",
+    "b hat den Wert = " + b, ",", "Und c hat den Wert = " + c
+);
