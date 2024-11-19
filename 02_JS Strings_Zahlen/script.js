ce0b51ba949400e8d218d70accad35400be84272
zeki57 <zeki.aksoy05@gmail.com>
2024-11-19 08:23:52 +0100

    02 JS Strings_Zahlen DONE

---
+++ 02_JS Strings_Zahlen/script.js
@@ -0,0 +1,16 @@
+let a = 17.4;
+let b = 3.9;
+
+result = a / b;
+
+console.log(result);
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + result);
+
+//Texte STRINGS
+
+let vorname = "Zeki";
+let nachname = "Aksoy";
+
+console.log("Ich Heiße " + vorname + " " + nachname);
+
