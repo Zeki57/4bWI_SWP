f66cf990d1729d9f07e040400841861ebb973294
zeki57 <zeki.aksoy05@gmail.com>
2024-11-26 09:26:02 +0100

    04_Schleifen DONE

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,38 @@
+
+for (let count = 7; count <= 45; count++) {
+    console.log(count);
+}
+
+console.log("Fertig");
+
+
+for (let count = 9; count <= 99; count++) {
+    console.log(count);
+}
+
+console.log("Fertig2");
+
+
+for (let count = 10; count >= 0; count--) {
+    console.log(count);
+}
+
+console.log("Fertig3");
+
+
+for (let count = 1234; count >= 1207; count--) {
+    console.log(count);
+}
+
+console.log("Fertig4");
+
+for (let count = 0; count <= 8; count++) {
+    console.log(count + " Zeki");
+}
+
+console.log("Fertig5");
+
+
+for (let count = 0; count <= 10; count++) {
+    console.log(count)
+}
