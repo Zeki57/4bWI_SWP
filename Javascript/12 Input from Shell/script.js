e5f3ac930e4a77fe1f23d881a8007ae3812eae6c
zeki57 <zeki.aksoy05@gmail.com>
2025-01-24 12:00:36 +0100

    12 Input from Shell DONE

---
+++ Javascript/12 Input from Shell/script.js
@@ -0,0 +1,45 @@
+import { resolve } from "path";
+
+import { createInterface } from "readline";
+
+const readline = createInterface({
+
+    input: process.stdin,
+
+    output: process.stdout,
+
+});
+
+const readLineAsync = () => {
+
+    return new Promise((resolve) => {
+
+        readline.question("", (userRes) => {
+
+            resolve(userRes);
+
+
+
+        });
+
+    });
+
+};
+
+
+console.log("Enter you name");
+let inputOfUser = await readLineAsync();
+console.log("Your name is" + inputOfUser);
+
+
+
+
+
+
+
+
+
+
+
+
+readline.close(); 
