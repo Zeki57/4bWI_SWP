f38f6a1e7343f8d8049d9a25cbdd9b8f453408c9
zeki57 <zeki.aksoy05@gmail.com>
2025-05-06 09:07:11 +0200

    filter einbauen und Demo FIlter DONE

--- REACT mit Functional Components/temp/vite-project/src/Components/PeopleCard.jsx
+++ REACT mit Functional Components/temp/vite-project/src/Components/PeopleCard.jsx
@@ -7,7 +7,7 @@ export default function PeopleCard({ name, title, imageUri }) {
                 <h2 className='font-bold text-xl'>{name}</h2>
                 <h4>{title}</h4>
             </div>
-            <div className='bg-green-400 w-full '>
+            <div className='bg-green-400 w-full border p-4 bg-green-600 '>
                 <img src={imageUri} alt="" className='object-cover w-full h-full' />
             </div>
 
--- REACT mit Functional Components/temp/vite-project/src/Components/PeopleContainer.jsx
+++ REACT mit Functional Components/temp/vite-project/src/Components/PeopleContainer.jsx
@@ -4,18 +4,38 @@ import PeopleCard from './PeopleCard'
 
 export default function PeopleContainer() {
     const [people, setPeople] = useState([]);
+    const [filteredPeople, setfilteredPeople] = useState([]);
 
     useEffect(() => {
         fetch("https://67b89a8c699a8a7baef4aa31.mockapi.io/Person").then(res => res.json().then(data => {
             setPeople(data);
+            setfilteredPeople(data);
         }));
     }, []);
 
+    const filterpeople = (filter) => {
+        let filtered = people.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()));
+        setfilteredPeople(filtered);
+    }
+
     return (
-        <div>
-            <h1>People</h1>
-            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4'>
-                {people.map(person => {
+
+        <div className=''>
+
+
+            <div className='fixed border mb-4 bg-white w-full h-28 p-8'>
+                <input
+                    className='border p-4'
+                    type="text"
+                    placeholder="Search"
+                    onChange={(el) => {
+                        console.log(el.target.value);
+                        filterpeople(el.target.value);
+
+                    }} />
+            </div>
+            <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 p-8 pt-32'>
+                {filteredPeople.map(person => {
                     return <PeopleCard name={person.name} title={person.jobtitle} imageUri={person.avatar} />
                 })}
             </div>
