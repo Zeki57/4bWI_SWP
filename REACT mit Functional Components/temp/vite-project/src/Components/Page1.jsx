b068d76d1d724a4b77b108ccb7385fbbb7df4b38
zeki57 <zeki.aksoy05@gmail.com>
2025-05-13 09:28:54 +0200

    first comit react router

---
+++ REACT mit Functional Components/temp/vite-project/src/Components/Page1.jsx
@@ -0,0 +1,16 @@
+export default function Page1() {
+    return (
+        <div>
+            <div>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <button />
+
+        </div>
+    )
+}
