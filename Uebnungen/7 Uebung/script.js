6bcdbc61010c0d51c2478e8e1726799424c4d228
zeki57 <zeki.aksoy05@gmail.com>
2025-06-02 22:11:34 +0200

    02062025 Uebungen

---
+++ Uebnungen/7 Uebung/script.js
@@ -0,0 +1,6 @@
+let zahl = 7;
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${zahl} x ${i} = ${zahl * i}`);
+}
---
+++ Uebnungen/8 Uebung/script.js
@@ -0,0 +1,5 @@
+let fruits = ["Apfel", "Banane", "Kirsche", "Mango"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
