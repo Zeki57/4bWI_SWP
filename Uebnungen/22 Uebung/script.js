9e044b00f07affed9ace520324086ff981b2153e
zeki57 <zeki.aksoy05@gmail.com>
2025-06-12 23:14:48 +0200

    12062025 Uebung

---
+++ Uebnungen/22 Uebung/script.js
@@ -0,0 +1,12 @@
+function zahl(arr) {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    console.log(max);
+}
+
+zahl([3, 7, 9, 18, 24, 12]);
