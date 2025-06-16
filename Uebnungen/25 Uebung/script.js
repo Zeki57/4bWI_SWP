2d52551cd3ede2ddc3fa40f2e44c532daa071eb7
zeki57 <zeki.aksoy05@gmail.com>
2025-06-16 11:52:18 +0200

    3 Codinggame

---
+++ Uebnungen/25 Uebung/script.js
@@ -0,0 +1,16 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+
+const A = 6;
+const B = 4;
+
+const dif = A - B;
+const sum = A + B;
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log("" + dif + sum);
